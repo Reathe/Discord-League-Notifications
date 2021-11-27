b01b2849c698e177fb42265b5c5a84b17238a2a1
Raf <bachourian@gmail.com>
2021-11-27 05:31:43 +0100

    indent spaces

--- src/database/database.py
+++ src/database/database.py
@@ -5,39 +5,39 @@ from player_account_link import PlayerAccountLink
 
 
 class MyDataBase(ABC):
-	def add(self, link: PlayerAccountLink):
-		self.set(link.name, link)
+    def add(self, link: PlayerAccountLink):
+        self.set(link.name, link)
 
-	@abstractmethod
-	def remove(self, key: Text):
-		pass
+    @abstractmethod
+    def remove(self, key: Text):
+        pass
 
-	@abstractmethod
-	def set(self, key: Text, link: PlayerAccountLink):
-		pass
+    @abstractmethod
+    def set(self, key: Text, link: PlayerAccountLink):
+        pass
 
-	@abstractmethod
-	def get(self, key: Text) -> PlayerAccountLink:
-		pass
+    @abstractmethod
+    def get(self, key: Text) -> PlayerAccountLink:
+        pass
 
-	def pop(self, key: Text) -> PlayerAccountLink:
-		link = self.get(key)
-		self.remove(key)
-		return link
+    def pop(self, key: Text) -> PlayerAccountLink:
+        link = self.get(key)
+        self.remove(key)
+        return link
 
-	def __getitem__(self, key: Text) -> PlayerAccountLink:
-		return self.get(key)
+    def __getitem__(self, key: Text) -> PlayerAccountLink:
+        return self.get(key)
 
-	def __setitem__(self, key, link: PlayerAccountLink):
-		self.set(key, link)
+    def __setitem__(self, key, link: PlayerAccountLink):
+        self.set(key, link)
 
-	def __delitem__(self, key: Text):
-		self.remove(key)
+    def __delitem__(self, key: Text):
+        self.remove(key)
 
-	@abstractmethod
-	def __iter__(self):
-		return self
+    @abstractmethod
+    def __iter__(self):
+        return self
 
-	@abstractmethod
-	def __len__(self):
-		pass
+    @abstractmethod
+    def __len__(self):
+        pass
--- src/main.py
+++ src/main.py
@@ -6,9 +6,9 @@ import traceback
 
 from discord.ext import commands
 
+from api.league_api import get_game, is_win, player_matchlist, request_puuid_byname
 from database.replit_database import MyReplitDB
 from keep_alive import keep_alive
-from api.league_api import get_game, is_win, player_matchlist, request_puuid_byname
 from messages import get_message
 from player_account_link import PlayerAccountLink
 
@@ -18,203 +18,203 @@ bot = commands.Bot(command_prefix='!lb ')
 
 
 def list_links() -> str:
-	link: PlayerAccountLink
-	res = '```python\n[\n'
-	for link in links_db:
-		res += link.__repr__() + ',\n'
-	res += ']```'
-	return res
+    link: PlayerAccountLink
+    res = '```python\n[\n'
+    for link in links_db:
+        res += link.__repr__() + ',\n'
+    res += ']```'
+    return res
 
 
 @bot.event
 async def on_ready():
-	await init_last_played_games()
-	while True:
-		try:
-			await asyncio.sleep(60)
-			await loop()
-		except Exception as e:
-			print(e)
+    await init_last_played_games()
+    while True:
+        try:
+            await asyncio.sleep(60)
+            await loop()
+        except Exception as e:
+            print(e)
 
 
 @bot.event
 async def on_command_error(ctx, error):
-	if isinstance(error, commands.CommandNotFound):
-		await ctx.send(f"No such command: {error}  (!lb help)")
-	elif isinstance(error, commands.MissingRequiredArgument):
-		await ctx.send(f"Missing an argument: {error}")
-		await ctx.send(f"```!lb {ctx.command} {ctx.command.signature}\n\n{ctx.command.help}```")
-	else:
-		raise Exception
+    if isinstance(error, commands.CommandNotFound):
+        await ctx.send(f"No such command: {error}  (!lb help)")
+    elif isinstance(error, commands.MissingRequiredArgument):
+        await ctx.send(f"Missing an argument: {error}")
+        await ctx.send(f"```!lb {ctx.command} {ctx.command.signature}\n\n{ctx.command.help}```")
+    else:
+        raise Exception
 
 
 @bot.command(name='list')
 async def _list(ctx):
-	"""
-	Shows the list of followed accounts
+    """
+    Shows the list of followed accounts
 
-	Example:
-		!lb list
-	"""
-	res = list_links()
-	await ctx.send(res)
+    Example:
+        !lb list
+    """
+    res = list_links()
+    await ctx.send(res)
 
 
 @bot.command()
 async def clear_list(ctx):
-	"""
-	Clears the list of accounts to follow
+    """
+    Clears the list of accounts to follow
 
-	Example:
-		!lb clear_list
-	"""
-	global links_db
-	link: PlayerAccountLink
-	for link in links_db:
-		del links_db[link.name]
-	await ctx.send('Cleared links')
+    Example:
+        !lb clear_list
+    """
+    global links_db
+    link: PlayerAccountLink
+    for link in links_db:
+        del links_db[link.name]
+    await ctx.send('Cleared links')
 
 
 @bot.command(name='del')
 async def _del(ctx, name):
-	"""
-		Deletes a player from the list
+    """
+        Deletes a player from the list
 
-		Notes:
-			-You HAVE to add the quotes "" around the name if it contains spaces !
+        Notes:
+            -You HAVE to add the quotes "" around the name if it contains spaces !
 
-		Example:
-			!lb del MyBro
-			!lb del "My Best Bro"
-	"""
-	global links_db
-	try:
-		links_db.pop(name)
-		await ctx.send(f'Deleted {name}!')
-	except KeyError:
-		await ctx.send(f'{name} not in list')
+        Example:
+            !lb del MyBro
+            !lb del "My Best Bro"
+    """
+    global links_db
+    try:
+        links_db.pop(name)
+        await ctx.send(f'Deleted {name}!')
+    except KeyError:
+        await ctx.send(f'{name} not in list')
 
 
 @bot.command()
 async def add(ctx, player_name, summoner_name, discord_id):
-	"""
-	Adds a new player to follow, and links its league account to discord id
+    """
+    Adds a new player to follow, and links its league account to discord id
 
-	Notes:
-		-You HAVE to add the quotes "" around the name or summuner name if it contains spaces !
-		-To get the user's Discord ID: enable 'developer mode' in discord settings under the 'advanced' tab,
-		then right-click the user and select 'copy identifier'
+    Notes:
+        -You HAVE to add the quotes "" around the name or summuner name if it contains spaces !
+        -To get the user's Discord ID: enable 'developer mode' in discord settings under the 'advanced' tab,
+        then right-click the user and select 'copy identifier'
 
-	Example:
-		!lb add MyBro "Bro Best Player" 121214596453912744
-	"""
-	if len(links_db) > 50:
-		await ctx.send('too many links, delete some')
-		return
+    Example:
+        !lb add MyBro "Bro Best Player" 121214596453912744
+    """
+    if len(links_db) > 50:
+        await ctx.send('too many links, delete some')
+        return
 
-	try:
-		new_link = PlayerAccountLink(player_name, await request_puuid_byname(summoner_name), discord_id)
-		if any(link == new_link for link in links_db):
-			await ctx.send('Error: link or name already in list')
-			return
-		new_link.last_game = (await player_matchlist(new_link.league_puuid))[0]
-		await bot.fetch_user(new_link.discord_id)
-		links_db.set(new_link.name, new_link)
-		await ctx.send(f'added new link: {links_db[new_link.name]}')
-	except Exception as e:
-		print(e, file=sys.stderr)
-		traceback.print_exc()
-		print(f'error add({player_name})')
-		await ctx.send('Error adding the link... Check names and ids or try again later')
+    try:
+        new_link = PlayerAccountLink(player_name, await request_puuid_byname(summoner_name), discord_id)
+        if any(link == new_link for link in links_db):
+            await ctx.send('Error: link or name already in list')
+            return
+        new_link.last_game = (await player_matchlist(new_link.league_puuid))[0]
+        await bot.fetch_user(new_link.discord_id)
+        links_db.set(new_link.name, new_link)
+        await ctx.send(f'added new link: {links_db[new_link.name]}')
+    except Exception as e:
+        """print(e, file=sys.stderr)
+        traceback.print_exc()
+        print(f'error add({player_name})')"""
+        await ctx.send('Error adding the link... Check names and ids or try again later')
 
-		m = type("", (), {})()
-		m.name = 'None'
-		raise commands.MissingRequiredArgument(m)
+        m = type("", (), {})()
+        m.name = 'None'
+        raise commands.MissingRequiredArgument(m)
 
 
 @bot.command()
 async def add_win_message(ctx, name, message):
-	"""
-	Adds a new message the user can receive when he wins!
+    """
+    Adds a new message the user can receive when he wins!
 
-	Notes:
-		-You HAVE to add the quotes "" around the name or message if it contains spaces !
+    Notes:
+        -You HAVE to add the quotes "" around the name or message if it contains spaces !
 
-	Example:
-		!lb add_win_message "My GIGA Bro" "Hahah told you you'd win!"
-		!lb add_win_message MyBro "Lucky one..."
-	"""
-	link = links_db.get(name)
-	if message in link.custom_message[True]:
-		await ctx.send(f'{name} can already receive this message when he wins.')
-		return
-	link.custom_message[True].append(message)
-	links_db[link.name] = link
-	await ctx.send(f'{name} can now receive this message when he wins.')
+    Example:
+        !lb add_win_message "My GIGA Bro" "Hahah told you you'd win!"
+        !lb add_win_message MyBro "Lucky one..."
+    """
+    link = links_db.get(name)
+    if message in link.custom_message[True]:
+        await ctx.send(f'{name} can already receive this message when he wins.')
+        return
+    link.custom_message[True].append(message)
+    links_db[link.name] = link
+    await ctx.send(f'{name} can now receive this message when he wins.')
 
 
 @bot.command()
 async def add_lose_message(ctx, name, message):
-	"""
-	Adds a new message the user can receive when he loses!
+    """
+    Adds a new message the user can receive when he loses!
 
-	Notes:
-		-You HAVE to add the quotes "" around the name or message if it contains spaces !
+    Notes:
+        -You HAVE to add the quotes "" around the name or message if it contains spaces !
 
-	Example:
-		!lb add_lose_message "My GIGA Bro" "Hahah told you you'd win... (everyone makes mistakes)"
-		!lb add_lose_message MyBro "Unlucky one..."
-	"""
-	link = links_db[name]
-	if message in link.custom_message[False]:
-		await ctx.send(
-			f'{name} can already receive this message when he loses.')
-		return
-	link.custom_message[False].append(message)
-	links_db[link.name] = link
-	await ctx.send(f'{name} can now receive this message when he loses.')
+    Example:
+        !lb add_lose_message "My GIGA Bro" "Hahah told you you'd win... (everyone makes mistakes)"
+        !lb add_lose_message MyBro "Unlucky one..."
+    """
+    link = links_db[name]
+    if message in link.custom_message[False]:
+        await ctx.send(
+            f'{name} can already receive this message when he loses.')
+        return
+    link.custom_message[False].append(message)
+    links_db[link.name] = link
+    await ctx.send(f'{name} can now receive this message when he loses.')
 
 
 async def init_last_played_games():
-	for link in links_db:
-		try:
-			link.last_game = (await player_matchlist(link.league_puuid))[0]
-		except Exception as e:
-			print(e, file=sys.stderr)
-			traceback.print_exc()
-			print('riot donne pas les stats (' + link.name + ')')
+    for link in links_db:
+        try:
+            link.last_game = (await player_matchlist(link.league_puuid))[0]
+        except Exception as e:
+            print(e, file=sys.stderr)
+            traceback.print_exc()
+            print('riot donne pas les stats (' + link.name + ')')
 
 
 async def loop():
-	global links_db
-	link: PlayerAccountLink
-	for link in links_db:
-		try:
-			# print(data)
-			match_list = await player_matchlist(link.league_puuid, int(time.time()) - 60 * 5)
-			if len(match_list) > 0:
-				gameId = match_list[0]
-				if gameId != link.last_game:
-					game = await get_game(gameId)
-					try:
-						user = await bot.fetch_user(link.discord_id)
-						mess = get_message(is_win(game, link.league_puuid), link, game)
-						await user.send(mess)
-						link.last_game = gameId
-						links_db[link.name] = link
-					except Exception as e:
-						print(e, file=sys.stderr)
-						traceback.print_exc()
-						print("Impossible d'envoyer à " + str(link.name))
+    global links_db
+    link: PlayerAccountLink
+    for link in links_db:
+        try:
+            # print(data)
+            match_list = await player_matchlist(link.league_puuid, int(time.time()) - 60 * 5)
+            if len(match_list) > 0:
+                gameId = match_list[0]
+                if gameId != link.last_game:
+                    game = await get_game(gameId)
+                    try:
+                        user = await bot.fetch_user(link.discord_id)
+                        mess = get_message(is_win(game, link.league_puuid), link, game)
+                        await user.send(mess)
+                        link.last_game = gameId
+                        links_db[link.name] = link
+                    except Exception as e:
+                        print(e, file=sys.stderr)
+                        traceback.print_exc()
+                        print("Impossible d'envoyer à " + str(link.name))
 
-		except Exception as e:
-			print(e, file=sys.stderr)
-			traceback.print_exc()
-			print('Riot game donne pas les stats (' + link.name + ')')
-			continue
+        except Exception as e:
+            print(e, file=sys.stderr)
+            traceback.print_exc()
+            print('Riot game donne pas les stats (' + link.name + ')')
+            continue
 
 
 if __name__ == '__main__':
-	print(list_links())
-	keep_alive()
-	bot.run(DISCORD_API_KEY)
+    print(list_links())
+    keep_alive()
+    bot.run(DISCORD_API_KEY)
